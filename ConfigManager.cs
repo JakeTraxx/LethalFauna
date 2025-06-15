fe83f715906d59052d452419a8cc91f8cda5d494
bcs4313 <bcs4313@g.rit.edu>
2025-06-15 02:23:37 -0400

    Add project files.

---
+++ ConfigManager.cs
@@ -0,0 +1,16 @@
+﻿using BepInEx.Configuration;
+
+namespace LethalFauna
+{
+    internal class ConfigManager
+    {
+        public static ConfigEntry<bool> enableSkunkBear;
+        public static ConfigEntry<bool> enableWatcherHarpy;
+
+        public static void Init()
+        {
+            enableSkunkBear = LethalFaunaMod.instance.Config.Bind("Skunk Bear Settings", "enableSkunkBear", true, "Enables spawning of Skunk Bears.");
+            enableWatcherHarpy = LethalFaunaMod.instance.Config.Bind("Watcher Harpy Settings", "enableWatcherHarpy", true, "Enables spawning of Watcher Harpys.");
+        }
+    }
+}
