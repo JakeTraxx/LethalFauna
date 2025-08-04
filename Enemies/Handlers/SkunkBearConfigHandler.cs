1f33b9e533eee6f84caa594bca1be9cda206addb
Ccode-lang <coop112097@gmail.com>
2025-08-03 17:43:47 -0700

    New loading system.

---
+++ Enemies/Handlers/SkunkBearConfigHandler.cs
@@ -0,0 +1,20 @@
+﻿using BepInEx.Configuration;
+using LethalFauna.Util;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LethalFauna.Enemies.Handlers
+{
+    internal class SkunkBearConfigHandler : ConfigHandler
+    {
+        public ConfigEntry<bool> enableSkunkBear;
+
+        public SkunkBearConfigHandler(ConfigFile config) : base(config)
+        {
+            enableSkunkBear = config.Bind("Skunk Bear Settings", "enableSkunkBear", true, "Enables spawning of Skunk Bears.");
+        }
+    }
+}
---
+++ Enemies/Handlers/WatcherHarpyHandler.cs
@@ -0,0 +1,29 @@
+﻿using LethalLib.Modules;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace LethalFauna.Enemies.Handlers
+{
+    internal class WatcherHarpyHandler
+    {
+        public static WatcherHarpyConfigHandler Config;
+
+        public WatcherHarpyHandler()
+        {
+            Config = new WatcherHarpyConfigHandler(LethalFaunaMod.instance.CreateConfig("watcherharpy"));
+            if (!Config.enableWatcherHarpy.Value)
+                return;
+
+            AssetBundle bundle = LethalFaunaMod.bundle;
+            var wh = bundle.LoadAsset<EnemyType>("Assets/LethalFauna/WatcherHarpy/WatcherHarpy.asset");
+            var watcherHarpyTN = bundle.LoadAsset<TerminalNode>("Assets/LethalFauna/WatcherHarpy/Bestiary/WatcherHarpyTN.asset");
+            var watcherHarpyTK = bundle.LoadAsset<TerminalKeyword>("Assets/LethalFauna/WatcherHarpy/Bestiary/WatcherHarpyTK.asset");
+            NetworkPrefabs.RegisterNetworkPrefab(wh.enemyPrefab);
+            LethalLib.Modules.Enemies.RegisterEnemy(wh, 100, Levels.LevelTypes.All, watcherHarpyTN, watcherHarpyTK);
+        }
+    }
+}
