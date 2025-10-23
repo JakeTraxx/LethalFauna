444e31da6172cf41c1d7b87fd5e5518a3112c29d
Brendan King <brendan@brendanking.com>
2025-10-22 21:08:43 -0400

    Fixed exceptions caused by hives being eaten

---
+++ Patches/RedLocustBeesPatch.cs
@@ -0,0 +1,33 @@
+﻿using HarmonyLib;
+
+namespace LethalFauna.Patches
+{
+    [HarmonyPatch(typeof(RedLocustBees))]
+    internal class RedLocustBeesPatch
+    {
+        // these two methods dont account for a hive being destroyed, so lets fix that
+        [HarmonyPatch("IsHiveMissing")]
+        [HarmonyPrefix]
+        static bool IsHiveMissingPatch(RedLocustBees __instance, ref bool __result)
+        {
+            if (__instance.hive == null)
+            {
+                __result = true;
+                return false;
+            }
+            return true;
+        }
+
+        [HarmonyPatch("IsHivePlacedAndInLOS")]
+        [HarmonyPrefix]
+        static bool IsHivePlacedAndInLOSPatch(RedLocustBees __instance, ref bool __result)
+        {
+            if (__instance.hive == null)
+            {
+                __result = false;
+                return false;
+            }
+            return true;
+        }
+    }
+}
